c19619f73976ce47897b71f1fe904e23231bfc82
x1stanzz <vova28032003@gmail.com>
2023-10-09 17:17:23 +0300

    Project created

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            todo:
+             1. Create classes named Book and Library that represent them
+             2. Create fields/methods inside those classes:
+                -- Book: Title, Author, ISBN, Year
+                -- Library: addBook(), showBooks(), findBook(), removeBook()
+             3. Create a method that will add books to the library
+             4. Create a method to show all books in the library
+             5. Create a method to find a book by its title
+             6. Create a method to remove a book
+             7. Make some test cases
+        * */
+    }
+}
