ae2a4060200ac3f8f9d9ccdcf9fbbfdf0ff488c8
x1stanzz <vova28032003@gmail.com>
2023-10-09 18:24:50 +0300

    Created a main program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,33 @@ public class Main {
              6. Create a method to remove a book
              7. Make some test cases
         * */
+        //Creating library to keep books
+        Library library = new Library();
+
+        //Adding books to the library
+        library.addBook(new Book("The Hobbit", "J.R.R. Tolkien","333333333", 1937));
+        library.addBook(new Book("The Great Gatsby","F.Scott Fitzgerald", "222222222", 1925));
+        library.addBook(new Book("The little prince", "Antoine de Saint-Exupery", "111111111", 1943));
+
+        //Checking what library contains
+        System.out.println("Library before changes: ");
+        library.showBooks();
+
+        //Finding book by its title
+        Book isBookFound = library.findBook("The Hobbit");
+        if(isBookFound != null){
+            System.out.println("The book was found: ");
+            library.showBookInfo(isBookFound);
+        } else{
+            System.out.println("The book was not found!");
+        }
+
+        //Attempt to remove a book from library
+        //If book is absent, the message will appear
+        String isBookRemoved = library.removeBook("111111111");
+        System.out.println(isBookRemoved + "\n");
+        //Checking library after changes
+        System.out.println("Library after changes: ");
+        library.showBooks();
     }
 }
